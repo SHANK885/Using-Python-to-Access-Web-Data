602107eaeb8bdfeb8344cf957b168f52c0563775
SHASHANK SHEKHAR <shashankshekhar885@gmail.com>
2019-01-29 23:13:20 +0530

    Week 3 Assignmet

---
+++ Week 3/socket1.py
@@ -0,0 +1,21 @@
+# -*- coding: utf-8 -*-
+"""
+Spyder Editor
+
+This is a temporary script file.
+"""
+
+import socket
+
+mysocket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+mysocket.connect(('www.py4e.org', 80))
+cmd = "GET http://data.pr4e.org/intro-short.txt HTTP/1.0\r\n\r\n".encode()
+mysocket.send(cmd)
+
+while True:
+    data = mysocket.recv(1024)
+    if len(data)<1:
+        break
+    print(data.decode(), end = '')
+
+mysocket.close()
