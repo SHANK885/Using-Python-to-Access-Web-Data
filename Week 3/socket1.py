4d1edcf5380c6a6488168cf3b36d97b92c03f667
SHASHANK SHEKHAR <shashankshekhar885@gmail.com>
2019-01-30 14:49:49 +0530

    Update socket1.py

--- Week 3/socket1.py
+++ Week 3/socket1.py
@@ -9,11 +9,11 @@ import socket
 
 mysocket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 mysocket.connect(('www.py4e.org', 80))
-cmd = "GET http://data.pr4e.org/intro-short.txt HTTP/1.0\r\n\r\n".encode()
+cmd = "GET http://www.pr4e.org/intro-short.txt HTTP/1.0\r\n\r\n".encode()
 mysocket.send(cmd)
 
 while True:
-    data = mysocket.recv(1024)
+    data = mysocket.recv(512)
     if len(data)<1:
         break
     print(data.decode(), end = '')
